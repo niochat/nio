d8657699bb8a1dbe3521ca36578ec16325f0d218
Kilian Koeltzsch <me@kilian.io>
2019-11-19 15:25:23 +0100

    Rename ContentView to RootView

--- Nio/SceneDelegate.swift
+++ Nio/SceneDelegate.swift
@@ -20,12 +20,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let rootView = RootView()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            window.rootViewController = UIHostingController(rootView: rootView)
             self.window = window
             window.makeKeyAndVisible()
         }
