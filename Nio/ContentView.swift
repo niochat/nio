d082995d971fe0f4307a76b7a3c2565a0be0bfe8
Kilian Koeltzsch <me@kilian.io>
2019-11-19 15:24:13 +0100

    Initial Commit

---
+++ Nio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nio
+//
+//  Created by Kilian Koeltzsch on 19.11.19.
+//  Copyright © 2019 Kilian Koeltzsch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
