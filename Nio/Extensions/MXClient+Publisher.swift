28266152975364cc06c49e82868eacf89134e99f
Kilian Koeltzsch <me@kilian.io>
2019-11-22 23:17:26 +0100

    Add publicrooms publisher

--- Nio/Extensions/MXClient+Publisher.swift
+++ Nio/Extensions/MXClient+Publisher.swift
@@ -15,4 +15,17 @@ extension MXRestClient {
             }
         }.eraseToAnyPublisher()
     }
+
+    func nio_publicRooms(onServer: String? = nil, limit: UInt? = nil) -> AnyPublisher<MXPublicRoomsResponse, Error> {
+        Future<MXPublicRoomsResponse, Error> { promise in
+            self.publicRooms(onServer: onServer, limit: limit) { response in
+                switch response {
+                case .failure(let error):
+                    promise(.failure(error))
+                case .success(let publicRoomsResponse):
+                    promise(.success(publicRoomsResponse))
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
 }
