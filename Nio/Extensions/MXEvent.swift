8388fe1d5ab91f0a7f91bf44bb86b8f92103f03b
Kilian Koeltzsch <me@kilian.io>
2019-12-02 19:19:47 +0100

    fix event timestamp extension

--- Nio/Extensions/MXEvent.swift
+++ Nio/Extensions/MXEvent.swift
@@ -3,7 +3,6 @@ import SwiftMatrixSDK
 
 extension MXEvent {
     var timestamp: Date {
-        // FIXME: This is wrong, but how does this work?
-        Date(timeIntervalSinceNow: -1 * Double(age))
+        Date(timeIntervalSince1970: TimeInterval(self.originServerTs / 1000))
     }
 }
