18b4351a67de98ca3249ae1573fa18c93129e630
Vincent Esche <regexident@gmail.com>
2019-12-08 23:47:06 +0100

    Remove `Color.random` extension

--- Nio/Extensions/Color+random.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-extension Color {
-    static var random: Color {
-        Color(red: .random(in: 0...1),
-              green: .random(in: 0...1),
-              blue: .random(in: 0...1))
-    }
-}
