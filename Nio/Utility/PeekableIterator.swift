de375fd760101dfe5958efa46c16f8737913d023
Vincent Esche <regexident@gmail.com>
2019-12-14 20:21:54 +0100

    Add documentation for `PeekableIterator`

--- Nio/Utility/PeekableIterator.swift
+++ Nio/Utility/PeekableIterator.swift
@@ -1,15 +1,20 @@
 import Foundation
 
+/// An iterator with a `peek()` that returns the next element
+/// without removing it without advancing the iterator.
 struct PeekableIterator<Iterator>: IteratorProtocol where Iterator: IteratorProtocol {
     typealias Element = Iterator.Element
 
     private var iterator: Iterator
     private var peeked: Element?
 
+    /// Creates a peekable iterator, wrapping `iterator`.
+    /// - Parameter iterator: the wrapped iterator
     init(_ iterator: Iterator) {
         self.iterator = iterator
     }
 
+    /// Advances the iterator and returns the next value.
     mutating func next() -> Element? {
         guard let peeked = peeked else {
             return iterator.next()
@@ -18,6 +23,7 @@ struct PeekableIterator<Iterator>: IteratorProtocol where Iterator: IteratorProt
         return peeked
     }
 
+    /// Returns the `next()` value without advancing the iterator.
     mutating func peek() -> Element? {
         if peeked == nil {
             peeked = iterator.next()
