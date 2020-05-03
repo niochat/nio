dd9c2d76aa674f8a23d603060554ceade194ea97
Kilian Koeltzsch <me@kilian.io>
2020-05-03 02:41:33 +0200

    Add AccountManager stub

---
+++ Nio/Data/Account/AccountManager.swift
@@ -0,0 +1,22 @@
+import Foundation
+import KeychainAccess
+
+// Implementation heavily inspired by [Messagerie](https://github.com/manuroe/messagerie).
+
+class AccountManager {
+    func all() -> [Account] {
+        fatalError()
+    }
+
+    func add(account: Account) {
+        fatalError()
+    }
+
+    func update(account: Account) {
+        fatalError()
+    }
+
+    func remove(account: Account) {
+        fatalError()
+    }
+}
