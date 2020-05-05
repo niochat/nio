1a68060eeed3ad603fdb5fc1254bcaf925262b5e
Vincent Esche <regexident@gmail.com>
2020-05-05 09:29:57 +0200

    Added 'NioKit' framework

---
+++ NioKit/NioKit.h
@@ -0,0 +1,19 @@
+//
+//  NioKit.h
+//  NioKit
+//
+//  Created by Vincent Esche on 5/5/20.
+//  Copyright © 2020 Kilian Koeltzsch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NioKit.
+FOUNDATION_EXPORT double NioKitVersionNumber;
+
+//! Project version string for NioKit.
+FOUNDATION_EXPORT const unsigned char NioKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NioKit/PublicHeader.h>
+
+
