52e7ca3168c1a83c8a1d4b3887f2e55323387fe9
Kilian Koeltzsch <me@kilian.io>
2020-07-01 18:15:05 +0200

    try and trigger swiftlint

--- Nio/RootView.swift
+++ Nio/RootView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct RootView: View {
     @EnvironmentObject var store: AccountStore
 
+    let a = 1
+
     var body: some View {
         switch store.loginState {
         case .loggedIn(let userId):
