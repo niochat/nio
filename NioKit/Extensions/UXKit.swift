8255244cbe9c5ba13942febb02cbdd7e4f2d88b2
Helge Heß <devteam@zeezide.de>
2021-03-05 16:31:51 +0100

    Add a few more things to UXKit
    Avoid having to change all the iOS SwiftUI.

--- NioKit/Extensions/UXKit.swift
+++ NioKit/Extensions/UXKit.swift
@@ -9,11 +9,67 @@
 #if os(macOS)
     import AppKit
 
-    public typealias UXImage = NSImage
+    public typealias UXColor      = NSColor
+    public typealias UXImage      = NSImage
+    public typealias UXEdgeInsets = NSEdgeInsets
+    public typealias UXFont       = NSFont
+
+    public enum UXFakeTraitCollection {
+        case current
+    }
+
+    public extension NSColor {
+      
+        @inlinable
+        func resolvedColor(with fakeTraitCollection: UXFakeTraitCollection)
+             -> UXColor
+        {
+            return self
+        }
+    }
+
+  #if canImport(SwiftUI)
+    import SwiftUI
+    
+    public enum UXFakeDisplayMode {
+        case inline, automatic, large
+    }
+    public enum UXFakeAutocapitalizationMode {
+        case none
+    }
+
+    @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+    public extension View {
+      
+        @inlinable
+        func navigationBarTitle<S: StringProtocol>(
+            _ title: S, displayMode: UXFakeDisplayMode = .inline
+        ) -> some View
+        {
+            self.navigationTitle(title)
+        }
+      
+        @inlinable
+        func autocapitalization(_ mode: UXFakeAutocapitalizationMode) -> Self {
+            return self
+        }
+    }
+  #endif
 #elseif canImport(UIKit)
     import UIKit
 
-    public typealias UXImage = UIImage
+    public typealias UXColor      = UIColor
+    public typealias UXImage      = UIImage
+    public typealias UXEdgeInsets = UIEdgeInsets
+    public typealias UXFont       = UIFont
+
+  #if canImport(SwiftUI)
+    import SwiftUI
+  
+    @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+    public extension View {
+    }
+  #endif
 #else
     #error("GNUstep not yet supported, sorry!")
 #endif
