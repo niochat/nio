9cb5e16c35ffea69ec3989033441455cf2a95e3b
Helge Heß <devteam@zeezide.de>
2021-03-07 14:40:20 +0100

    Scoping and assert's
    A few more private's and assert's to check for
    expected conditions.

--- Nio/RootView.swift
+++ Nio/RootView.swift
@@ -3,15 +3,23 @@ import SwiftUI
 import NioKit
 
 struct RootView: View {
-    @EnvironmentObject var store: AccountStore
+    @EnvironmentObject private var store: AccountStore
+  
+    private var homeserverURL: URL {
+        // Can this ever be nil? And if so, what happens with the default fallback?
+        assert(store.client != nil)
+        let configuredURL = store.client?.homeserver.flatMap(URL.init)
+        assert(configuredURL != nil)
+        return configuredURL ?? HomeserverKey.defaultValue
+    }
 
     var body: some View {
         switch store.loginState {
         case .loggedIn(let userId):
             RecentRoomsContainerView()
                 .environment(\.userId, userId)
-                // Can this ever be nil? And if so, what happens with the default fallback?
-                .environment(\.homeserver, (store.client?.homeserver.flatMap(URL.init)) ?? HomeserverKey.defaultValue)
+                .environment(\.homeserver, homeserverURL)
+          
         case .loggedOut:
             LoginContainerView()
 
@@ -23,11 +31,10 @@ struct RootView: View {
                 Spacer()
                 Text(error.localizedDescription)
                 Spacer()
-                Button(action: {
-                    self.store.loginState = .loggedOut
-                }, label: {
+                Button(action: { self.store.loginState = .loggedOut }) {
                     Text(L10n.Login.failureBackToLogin)
-                }).padding()
+                }
+                .padding()
             }
         }
     }
