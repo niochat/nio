f829a6517295cca32ae36e7237829a674dec1e2f
Helge Heß <devteam@zeezide.de>
2021-03-16 11:54:08 +0100

    Mio: New SFSymbol mappings
    Mio uses more SF icons already. Maybe iOS should too.

--- Nio/Shared Views/SFSymbol.swift
+++ Nio/Shared Views/SFSymbol.swift
@@ -1,8 +1,13 @@
 import SwiftUI
 
 enum SFSymbol: String, View {
-    case typing = "scribble.variable"
-    case close = "xmark"
+    case typing          = "scribble.variable"
+    case close           = "xmark"
+
+    case newConversation = "square.and.pencil"
+    case settings        = "gear"
+    case send            = "paperplane"
+    case attach          = "paperclip"
 
     var body: some View {
         Image(systemName: self.rawValue)
