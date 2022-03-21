b63826f918f7ebb4d2ff88f029a3abc5e4d4e07f
Finn Behrens <me@kloenk.dev>
2022-03-21 17:43:28 +0100

    Initial Commit

---
+++ nio/nioApp.swift
@@ -0,0 +1,17 @@
+//
+//  nioApp.swift
+//  nio
+//
+//  Created by Finn Behrens on 21.03.22.
+//
+
+import SwiftUI
+
+@main
+struct nioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
